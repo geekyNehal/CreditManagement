c30e6f6fa5401cc61825da0a061ada5c3c399120
Nehal Kumar Singh <nehaljohn91@gmail.com>
2019-06-01 16:50:16 +0530

    Transaction Table database created

--- app/src/main/java/com/geekynehal/creditmanagement/MainActivity.java
+++ app/src/main/java/com/geekynehal/creditmanagement/MainActivity.java
@@ -2,11 +2,11 @@ package com.geekynehal.creditmanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView myCredit;
     Button transferTable,viewUsers,transferCredit;
     @Override
@@ -17,6 +17,25 @@ public class MainActivity extends AppCompatActivity
         transferTable=findViewById(R.id.transfer_table_button);
         viewUsers=findViewById(R.id.view_users_button);
         transferCredit=findViewById(R.id.transfer_credit_button);
+        transferTable.setOnClickListener(this);
+        viewUsers.setOnClickListener(this);
+        transferCredit.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view)
+    {
+        switch(view.getId())
+        {
+            case R.id.transfer_table_button:
+
+                break;
+
+            case R.id.view_users_button:
+                break;
+
+            case R.id.transfer_credit_button:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/geekynehal/creditmanagement/TransactionDatabase.java
@@ -0,0 +1,6 @@
+package com.geekynehal.creditmanagement;
+
+public class TransactionDatabase
+{
+
+}
