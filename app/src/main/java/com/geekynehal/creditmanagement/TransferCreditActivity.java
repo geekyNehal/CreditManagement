5d7ce6ac618adcbebbdc18f944f5944d1f310ba8
Nehal Kumar Singh <nehaljohn91@gmail.com>
2019-06-03 00:09:53 +0530

    user details error resolved

--- app/src/main/java/com/geekynehal/creditmanagement/TransferCreditActivity.java
+++ app/src/main/java/com/geekynehal/creditmanagement/TransferCreditActivity.java
@@ -2,12 +2,26 @@ package com.geekynehal.creditmanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class TransferCreditActivity extends AppCompatActivity {
-
+    String name;
+    Spinner spinner_from,spinner_to;
+    Button submit;
+    EditText amt;
+    int amount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer_credit);
+        Bundle bundle=getIntent().getExtras();
+        name=bundle.getString("name");
+        spinner_from=findViewById(R.id.spinner_from);
+        spinner_to=findViewById(R.id.spinner_to);
+        submit=findViewById(R.id.submit);
+        amt=findViewById(R.id.amount_text);
+        amount=Integer.parseInt(amt.getText().toString());
     }
 }
--- app/src/main/java/com/geekynehal/creditmanagement/UserDetails.java
+++ app/src/main/java/com/geekynehal/creditmanagement/UserDetails.java
@@ -1,5 +1,6 @@
 package com.geekynehal.creditmanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -31,6 +32,9 @@ public class UserDetails extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View view) {
-
+        Intent intent=new Intent();
+        intent.setClass(getApplicationContext(),TransferCreditActivity.class);
+        intent.putExtra("name",name);
+        startActivity(intent);
     }
 }
